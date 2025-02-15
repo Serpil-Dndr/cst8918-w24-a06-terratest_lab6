3e4ad1d19d3f3f9132998d26fb5d7652ee25679c
Serpil-Dndr <serpil.dndrca@gmail.com>
2025-02-14 21:03:39 -0500

    first commit

---
+++ test/azure_webserver_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// You normally want to run this under a separate "Testing" subscription
+// For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
+var subscriptionID string = "63853715-8f78-4ec7-832a-fcdb77d8e567"
+
+func TestAzureLinuxVMCreation(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		// The path to where our Terraform code is located
+		TerraformDir: "../",
+		// Override the default terraform variables
+		Vars: map[string]interface{}{
+			"labelPrefix": "dund0021",
+		},
+	}
+
+	defer terraform.Destroy(t, terraformOptions)
+
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the value of output variable
+	vmName := terraform.Output(t, terraformOptions, "vm_name")
+	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
+
+	// Confirm VM exists
+	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	// Confirm NIC exists and is connected to the VM
+	nicList := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
+	assert.NotEmpty(t, nicList, "No NICs found for VM")
+
+	// Confirm the VM is running the correct Ubuntu version
+	expectedVersion := "22.04" 
+	vmImage := azure.GetVirtualMachineImage(t, vmName, resourceGroupName, subscriptionID)
+	assert.Equal(t, expectedVersion, vmImage.Version, "VM is not running the expected Ubuntu version")
+}
